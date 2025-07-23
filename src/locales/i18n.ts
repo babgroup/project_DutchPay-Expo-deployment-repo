9938fdf32ffab9bfa54e8a67e3a650483f7d36f6
Bacococc <pphj7353@naver.com>
2025-07-23 16:03:06 +0900

    🔨 refactor : import 경로 수정

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslations from './en/delivery.json';
-import koTranslations from './ko/delivery.json';
+import enTranslations from './en/delivery-home.json';
+import koTranslations from './ko/delivery-home.json';
 
 i18n
   .use(initReactI18next)
