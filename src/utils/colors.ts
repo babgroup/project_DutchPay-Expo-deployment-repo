6387957b5ddcf2b47585268a9a356fd471bd5f50
Bacococc <pphj7353@naver.com>
2025-07-23 16:04:41 +0900

    ✨ feat : hex 문자열을 반환하는 헬퍼 함수

---
+++ src/utils/colors.ts
@@ -0,0 +1,18 @@
+export const getStatusColor = (status: string) => {
+  switch (status) {
+    case 'orange':
+      return '#FC9533'; // 메인 주황색
+    
+    case 'gray':
+      return '#e7e7e7ff'; // 연회색(div의 배경 등)
+
+    case 'dark-gray':
+      return '#9b9b9bff'; // 기본 회색(placeholder, 비활성화된 버튼 등)
+
+    case 'white':
+      return '#FFFFFF'; // 흰색(배경 등)
+
+    case 'black':
+      return '#000000'; // 검정색
+  }
+};
